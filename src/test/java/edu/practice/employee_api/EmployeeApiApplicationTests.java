83da6bdde36fa571d36a979e799d3ad5e4ed6a3f
SenX51 <Arsemenov51@yandex.ru>
2024-06-24 20:18:58 +0300

    CREATE: Java Neoflex Practice Project Develop API to work with employees

---
+++ src/test/java/edu/practice/employee_api/EmployeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.practice.employee_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
